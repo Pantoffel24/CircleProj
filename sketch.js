dbed7c5af557e1493bb063a8a54aaf8b061d19ff
Christoffel <christischoeman@gmail.com>
2025-12-21 10:52:28 +0200

    Circle stops spinning

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@
 //the segments themselves are stationary
 
 let angle = 0; 
+let speed = 0.5; //base speed
 
 function setup(){
     //creates the canvas
@@ -14,6 +15,32 @@ function setup(){
 function draw(){
     //Paint the background black
     background(0);
+
+    //Sensor logic
+    let mouseA = getCorrectedMouseAngle();
+    let distFromCentre = dist(mouseX, mouseY, width/2, height/2);
+    let isHovering = false;
+
+    //Check if the mouse is on the ring 
+    if(distFromCentre>90 && distFromCentre<120){
+        //Check if mouse is on a specific segment
+        if((mouseA>0 && mouseA<90) || 
+            (mouseA>120 && mouseA<200)||
+            (mouseA>230 && mouseA<330)) {
+                isHovering = true;
+            } 
+    }
+
+    //new physics block
+    if(isHovering){
+        speed = 0;
+        cursor(HAND); //show pointer idk
+    } else {
+        speed = 0.5;
+        cursor(ARROW) //show arrow (what)
+    }
+
+    push(); //starts isolation
 
     noFill();
     stroke(255);
@@ -34,5 +61,29 @@ function draw(){
     //segment 3
     arc(0,0,d,d,230,330);
 
-    angle = angle + 0.5;
+    pop();
+
+    angle = angle + speed;
+}
+
+//helper function thingy
+function getCorrectedMouseAngle(){
+    //1. Calculate the angle from the centre
+    let rawAngle = degrees(atan2(mouseY - height/2, mouseX - width/2));
+
+    //2. Negative raw angle correction
+    if(rawAngle<0){
+        rawAngle = rawAngle + 360;
+    }
+
+    //3. Subtract the circles rotation (un-spin)
+    let correctedAngle = rawAngle - angle;
+
+    //4. Keep it within the 0-360 range
+    correctedAngle = correctedAngle%360;
+    if(correctedAngle<0){
+        correctedAngle = correctedAngle + 360;
+    }
+
+    return correctedAngle;
 }
